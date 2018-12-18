f60c65ee280f9ba1a623fe41c7af49e21a2ac991
Caleb Gammon <calebwgammon@gmail.com>
2018-12-18 16:09:15 -0500

    first commit

---
+++ src/components/feedback-test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    shallow
+} from 'enzyme';
+import Feedback from './feedback';
+
+describe('<Feedback />', () => {
+    it('Renders without crashing', () => {
+        shallow( < Feedback / > );
+    });
+});
+
+it('Renders something', () = {
+            let TEST_FEEDBACK = 'You are listening to a game!';
+
+            let wrapper = shallow( < Feedback feedback = {
+                    TEST_FEEDBACK
+                }
+                />);
+                expect(wrapper.contains(TEST_FEEDBACK)).toEqual(true);
+            });
